da29802f4353599b2d1028e8d74873d73d1c7997
cuhs <alexhu00374@gmail.com>
2024-11-30 12:47:30 -0800

    made click to add markers work properly

--- react/src/CustomMap.js
+++ react/src/CustomMap.js
@@ -15,10 +15,23 @@ const CustomMap = () => {
     setPlaceMarker(!placeMarker);
   };
 
-  const handleMapClick = (event) => {
-    if (placeMarker) {
-      const newMarker = { lat: event.latLng.lat(), lng: event.latLng.lng() };
-      setMarkers([...markers, newMarker]);
+  const handleMapClick = async (event) => {
+    console.log("in handleMapClick", event.detail.latLng)
+    
+    const newMarker = { lat: event.detail.latLng.lat, lng: event.detail.latLng.lng };
+    setMarkers([...markers, newMarker]);
+    console.log(newMarker)
+    try {
+      await axios.post("http://localhost:5001/v1/add-location", {
+        name: "New Location", // Update with the actual location name
+        category: "UCLA buildings", // Update with the actual category
+        user: "defaultUser", // Update with the actual user
+        lng: newMarker.lng,
+        lat: newMarker.lat
+      });
+      console.log("Location added successfully.");
+    } catch (error) {
+      console.error("Error adding location:", error);
     }
   };
 
@@ -31,7 +44,6 @@ const CustomMap = () => {
         lat: parseFloat(location.location_lattitude),
         lng: parseFloat(location.location_longitude)
       }));
-      console.log("fetchedMarkers: ", fetchedMarkers)
       setMarkers(
         fetchedMarkers
       );
@@ -57,7 +69,7 @@ const CustomMap = () => {
         <Map
           defaultZoom={16}
           defaultCenter={position}
-          onClick={handleMapClick}>
+          onClick={placeMarker ? handleMapClick : null}>
           {markers.map((marker, index) => (
             <Marker key={index} position={marker}></Marker>
           ))}
